1ea61cbad22a5f7b9a579c234e25cd3ffb57bc3e
bunny <bunnyeuphie@gmail.com>
2022-07-27 17:57:19 -0400

    termine mi tarea

---
+++ codigo.js
@@ -0,0 +1,16 @@
+console.log("Hello Darling");
+
+document.write(`<h1 style="color: rgb(255, 142, 194)"> ♡ Bunny & Katsuo ♡ </h1>`)
+
+document.write("<h3> ໒꒰ྀི˵ˊ ᵕ ˋ˵꒱ྀི১ Siempre juntos ~ ♡ </h3>")
+
+document.write(` <p> Holi ~ Soy tu conejita Bunny y esta es mi primera página, gracias por explicarme, te amo ♡ </p>`) 
+
+document.write(`<img style="margin-left: 30px ;" width="200px" src="https://i.pinimg.com/736x/ab/f8/09/abf809ef40c4e2acae1d4b1f9f76d979.jpg" alt="Icon lindo de Fluttershy">`)
+
+document.write(`<img style="margin-right: 60px ; margin-left: 30px;"  width="200px" 
+src="https://64.media.tumblr.com/d07d3643dd7be20d8e07cf2210a13021/tumblr_pl30oq7E1C1x46o3mo9_1280.jpg" alt="yo con corona de flores">`)
+
+document.write(`<img style="margin-right: 1000px ;"  width="500px"src="https://i.imgur.com/m6FIJ9N.gif" alt="nosotros dandonos besitos">`)
+
+document.write(`<p> Me gusta mucho pasar tiempo contigo, eres el mejor ~ (ꈍᴗꈍ)ε｀*) chuu ♡</p>`)
